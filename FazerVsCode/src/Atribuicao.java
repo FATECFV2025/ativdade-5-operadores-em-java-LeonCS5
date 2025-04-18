1f377dcde65c63478b870df6185d363f2c49a7d7
Leonam Candido <leonamcandido80@gmail.com>
2025-04-17 23:22:10 -0300

    third commit

--- FazerVsCode/src/Atribuicao.java
+++ FazerVsCode/src/Atribuicao.java
@@ -2,6 +2,10 @@ public class Atribuicao {
     public static void main(String[] args) {
         // Simple assignment
         int a = 10;
-
+        float b = 10.5f;
+        String texto = "POO";
+        a = 100;
+        boolean c = true;
+        double d = 100.5;
     }
 }
